d1b5a84e57aad79366ddb627c4aebe3e3368ef20
Emanuel Camarena <emanuel@Emanuels-MacBook-Air.local>
2022-08-07 17:51:08 -0500

    Buble sort, insertion sort and bynary search

--- AlgoExpert/BinarySearch/main.py
+++ AlgoExpert/BinarySearch/main.py
@@ -1,11 +1,21 @@
 def binarySearch(array, target):
     # Write your code here. 
-    found = 0
-    found = map(lambda target:[0] if array[0] == target else -1,array)
-    print(found)
-    return found
+    first_position = 0
+    last_position = len(array) - 1
+
+    while first_position <= last_position:
+        midpoint_position = first_position + (last_position - first_position) // 2
+        midpoint_value = array[midpoint_position] 
+        if midpoint_value == target:
+            return midpoint_position
+        elif target < midpoint_value:
+            last_position = midpoint_position -1
+        else:
+            first_position = midpoint_position + 1
+    return -1
+
 
 if __name__ == '__main__':
-    array = [0,1,21,45,45,61,71,72,73]
+    array = [0,1,21,33,45,45,61,71,72,73]
     target = 33
     print(binarySearch(array,target))
--- AlgoExpert/Buble Sort/main.py
+++ AlgoExpert/Buble Sort/main.py
@@ -1,3 +1,14 @@
-def bubbleSort(array):
-    for index in array:
-        pass
+def bubbleSort(unsorted_array):
+    index_lenght = len(unsorted_array) - 1
+    sorted = False
+
+    while not sorted:
+        sorted = True
+        for i in range(0,index_lenght):
+            if unsorted_array[i] > unsorted_array[i+1]:
+                sorted = False
+                unsorted_array[i] , unsorted_array[i+1] = unsorted_array[i+1] , unsorted_array[i]
+    return unsorted_array
+
+if __name__ == '__main__':
+    print(bubbleSort([4,6,8,3,9,10,1,3]))
